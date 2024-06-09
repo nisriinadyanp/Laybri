22bc58cbe3fd516f954463c3232c2401a482c1d7
safiramikhareta <136241512+safiramikhareta@users.noreply.github.com>
2024-06-09 14:51:15 +0700

    Update v_TambahLukisan.php

--- projek/v_TambahLukisan.php
+++ projek/v_TambahLukisan.php
@@ -4,28 +4,91 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Tambah Lukisan</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="hf.css">
+        <link rel="stylesheet" href="v_edit.css">
     </head>
     <body>
-        <h2>Yuk, Tambah Lukisan!</h2>
-        <form action="" method="post" name="tLukisan" enctype="multipart/form-data">
-                Judul Lukisan : <input type="text" name="judulLukisan"><br><br>
-                Nama Pelukis : <input type="text" name="namaPelukis"><br><br>
-                Deskrpsi Karya : <input type="text" name="deskripsiKarya"><br><br>
-                
-                <input type="file" name="addGambar" value="Tambah Foto">
-                <input id="tombol" type="submit" name="tLukisan" value="Tambah">
-        </form>
+        <!--Header -->
+        <header class="header">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <h5 class="MeJatim_header"><b>MeJatim</b></h5>
+                </div>
+                <div class="col-md-6 text-right">
+                    <button class="btn btn-outline-light profile-button">
+                        <i class="bi-person-circle"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="konten text-center">
+
+        <div class="judul">
+            <h5><b>Yuk, Tambah Lukisan!</b></h5> 
+        </div>
+
+        <div class="formulir">
+            <form action="" method="post" name="tLukisan" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="judulLukisan">Judul Lukisan</label>
+                    <input type="text" name="judulLukisan" id="judulLukisan" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="pelukis">Nama Pelukis</label>
+                    <input type="text" name="namaPelukis"  id="pelukis" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="deskripsi">Deskripsi Karya</label>
+                    <input type="text" name="deskripsiKarya" id="deskripsi" required>
+                </div>
+
+                <div class="form-file text-center">
+                    <label for="addGambar" class="addGambar">Tambah Foto</label><br>
+                    <input type="file" name="addGambar" id="addGambar" required>
+                </div>
+
+                <input class="tombol" type="submit" name="tLukisan" value="Tambah">
+
+            </form>
+        </div>
+
+    </div>   
 
         <?php
-            include_once("c_MeJatim.php");
-            
+            include_once("c_Mejatim.php");
+
             if(isset($_POST['tLukisan'])) {
-                $main = new c_MeJatim();
+                $main = new c_Mejatim();
                 $main->add_lukisan();
             }
         ?>
+
+    <!--Footer -->
+    <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 text-left">
+                        <h5 class="Mejatim_footer"><b>MeJatim</b></h5>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <p class="copyright">&copy; MeJatim 2024</p>
+                    </div>
+                    <div class="social col-md-4 text-right">
+                        <p class="sosmed">Media Sosial Kami</p>
+                        <ul class="social-media list-inline">
+                            <li class="list-inline-item"><a href="#"><i class="bi-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="#"><i class="bi-envelope"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
