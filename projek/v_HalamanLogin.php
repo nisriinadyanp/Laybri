bab61056e00c84a5b164d5d842e969a14b09bca6
safiramikhareta <136241512+safiramikhareta@users.noreply.github.com>
2024-06-11 22:43:59 +0700

    Update v_HalamanLogin.php

--- projek/v_HalamanLogin.php
+++ projek/v_HalamanLogin.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin MeJatim</title>
-    <!-- Include Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body class="bg-gray-200">
